ed80d292945bd441ef5f76ab5405342b400f8b49
USER <dogreed69@gmail.com>
2026-03-30 11:38:02 +0545

    middeware simple concept

---
+++ crud web api/Middleware/RequestLoggingMiddleware.cs
@@ -0,0 +1,38 @@
+﻿namespace crud_web_api.Middleware
+{
+	public class RequestLoggingMiddleware
+	{
+
+		private readonly RequestDelegate _next;
+		public RequestLoggingMiddleware(RequestDelegate next)
+		{
+			_next = next;
+		}
+		public async Task InvokeAsync(HttpContext context)
+		{
+			if (context.Request.Path.StartsWithSegments("/api/works"))
+			{
+				context.Response.StatusCode = StatusCodes.Status403Forbidden;
+				await context.Response.WriteAsync("Access to this path is forbidden.");
+				return;
+			}
+			await _next(context);
+
+			await Herke(context);
+		}
+
+
+
+		public async Task Herke(HttpContext context)
+		{
+			if(context.Request.Path.StartsWithSegments("/api/task"))
+			{
+				// now here logic for checking if user is authenticated or not
+				await context.Response.WriteAsync("Login First");
+				return;
+			}
+		}
+
+
+	}
+}
--- crud web api/Program.cs
+++ crud web api/Program.cs
@@ -27,7 +27,7 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
-
+app.UseMiddleware<crud_web_api.Middleware.RequestLoggingMiddleware>();
 app.MapControllers();
 
 app.Run();
